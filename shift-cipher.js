fc47db4b71890f1dba12187f8d6532ca64b8f4c1
cybergrrl <scout.burghardt@gmail.com>
2021-10-04 17:41:55 +0100

    added decrypt method

--- shift-cipher.js
+++ shift-cipher.js
@@ -21,7 +21,29 @@ class ShiftCipher {
       }
       return cipher.join('')
     }
+
+    decrypt() {
+      let decipher = [];
+      this._string = this._string.toLowerCase();
+      for (let i = 0; i < this._string.length; i++) {
+        if (this._string.charCodeAt(i) > 96 && this._string.charCodeAt(i) < 123) {
+          let new_pos = this._string.charCodeAt(i) - this._number;
+          if (new_pos < 97) {
+            new_pos += 26
+          }
+          decipher.push(String.fromCharCode(new_pos))
+             
+        } else {
+          decipher.push(String.fromCharCode(this._string.charCodeAt(i)))
+        }
+      }
+      return decipher.join('')
+    }
   }
   
   const mystring = new ShiftCipher("h-elloz", 1)
+  const encryptedstring = new ShiftCipher("I-FMMPA", 1)
+
+  
   console.log(mystring.encrypt())
+  console.log(encryptedstring.decrypt())
